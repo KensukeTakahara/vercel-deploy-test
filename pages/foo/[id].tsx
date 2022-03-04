b62361d745fd6fe847854b0d2ab321d8bf9524c2
Kensuke Takahara <zawcjk@gmail.com>
2022-03-04 09:01:24 +0900

    comment out

--- pages/foo/[id].tsx
+++ pages/foo/[id].tsx
@@ -1,38 +1,39 @@
-import type { NextPage, GetStaticProps } from "next";
+// import type { NextPage, GetStaticProps } from "next";
+import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
-export const getStaticProps: GetStaticProps = async () => {
-  console.log("foo getStaticProps 1");
-  const data = await fetch("https://www.yahoo.co.jp/").then((res) =>
-    res.text()
-  );
-  console.log("foo getStaticProps 2");
+// export const getStaticProps: GetStaticProps = async () => {
+//   console.log("foo getStaticProps 1");
+//   const data = await fetch("https://www.yahoo.co.jp/").then((res) =>
+//     res.text()
+//   );
+//   console.log("foo getStaticProps 2");
 
-  if (data.length > 1000) {
-    return {
-      redirect: {
-        permanent: false, // 永続的なリダイレクトかどうか
-        destination: "/bar", // リダイレクト先
-        // destination: 'https://example.com/' // 別サイトでも指定可能
-      },
-    };
-  }
+//   if (data.length > 1000) {
+//     return {
+//       redirect: {
+//         permanent: false, // 永続的なリダイレクトかどうか
+//         destination: "/bar", // リダイレクト先
+//         // destination: 'https://example.com/' // 別サイトでも指定可能
+//       },
+//     };
+//   }
 
-  console.log("foo getStaticProps 3");
-  return {
-    props: { data: "empty" }, // will be passed to the page component as props
-  };
-};
+//   console.log("foo getStaticProps 3");
+//   return {
+//     props: { data: "empty" }, // will be passed to the page component as props
+//   };
+// };
 
-export async function getStaticPaths() {
-  console.log("foo getStaticPaths");
-  return {
-    paths: [],
-    // Enable statically generating additional pages
-    // For example: `/posts/3`
-    fallback: true,
-  };
-}
+// export async function getStaticPaths() {
+//   console.log("foo getStaticPaths");
+//   return {
+//     paths: [],
+//     // Enable statically generating additional pages
+//     // For example: `/posts/3`
+//     fallback: true,
+//   };
+// }
 
 const Foo: NextPage = () => {
   console.log("foo");
